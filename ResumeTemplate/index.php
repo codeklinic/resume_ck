971a7e5c9b955d8c9e4a22c18fa72993f36da389
mindfield <52530155+mindfield@users.noreply.github.com>
2019-07-19 12:13:32 +0000

    Add files via upload
    The initial version of my first resume 
    It's subject to modifications

---
+++ ResumeTemplate/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resume Website Template</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<!-- ----------------Header Section ------------------------------>
+
+	<section id="header">
+		<div class="container text-center">
+			<div class="user-box">
+				<img src="images/user1.jpg">
+				<h1>Isaac Azameti</h1>
+				<p>Freelance Back End Developer</p>
+			</div>
+		</div>
+		<div class="scroll-btn">
+			<div class="scroll-bar">
+				<a href="#about"><span></span></a>
+			</div>
+		</div>
+	</section>
+
+
+
+<!-----------------User-Info Section----------------------------->
+
+	<section id="user-info">
+		<div class="nav-bar">
+			<nav class="navbar navbar-expand-lg">
+				<a class="navbar-brand" href="#"> <img src="images/mindfield.png"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"aria-label="Toggle navigation">
+					<i class="fa fa-bars"></i>
+					<span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarNav">
+			    	<ul class="navbar-nav ml-auto">
+			    		<li class="nav-item "><a class="nav-link" href="#top"> HOME</a></li>
+						<li class="nav-item"><a class="nav-link" href="#about"> ABOUT ME</a></li>
+						<li class="nav-item"><a class="nav-link" href="#resume"> RESUME</a></li>
+						<li class="nav-item"><a class="nav-link" href="#services"> SERVICES</a></li>
+						<li class="nav-item"><a class="nav-link" href="#contact"> CONTACT</a></li>
+					</ul>
+				</div>
+			</nav>
+    	</div>
+
+
+
+	<!----------------About---------------------------------->
+
+		<div class="about container" id="about">
+			<div class="row">
+				<div class="col-md-6 text-center"><img src="images/user2.jpg" class="profile-img"></div>
+				<div class="col-md-6">
+					<h3> WHO AM I ? </h3>
+					<p> Widgets were originally designed to provide a simple and easy-to-use way of giving design and structure control of the WordPress
+					Theme to the user, which is now available on properly “widgetized” WordPress Themes to include the header, footer, and elsewhere in the WordPress design and structure.</p>
+					<div class="skills-bar">
+						<p> Java Desktop Applications </p>
+						<div class="progress"><div class="progress-bar" style="width: 60%;"> 60% </div></div>
+						<p> Web Applications </p>
+						<div class="progress"><div class="progress-bar" style="width: 90%;"> 90% </div></div>
+						<p> Android Applications </p>
+						<div class="progress"><div class="progress-bar" style="width: 40%;"> 40% </div></div>
+						<p> Bootstrap </p>
+						<div class="progress"><div class="progress-bar" style="width: 70%;"> 70% </div></div>
+						<p> Wordpress </p>
+						<div class="progress"><div class="progress-bar" style="width: 65%;"> 65% </div></div>
+					</div>
+				</div>
+			</div>
+		</div>	
+
+
+
+<!----------------Social Icons---------------------------------->
+
+		<div class="social-icons">
+			<ul>
+				<a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+				<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+				<a href="#"><li><i class="fa fa-medium"></i></li></a>
+				<a href="#"><li><i class="fa fa-github"></i></li></a>
+				<a href="#"><li><i class="fa fa-behance"></i></li></a>
+				<a href="#"><li><i class="fa fa-facebook"></i></li></a>
+				<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+			</ul>	
+		</div>
+
+
+
+	<!----------------Resume---------------------------------->
+		<div class="resume" id="resume">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<h3 class="text-center"> My Experiences </h3>
+						<ul class="timeline">
+							<li>
+								<h4><span> 2019 - </span> Web Developer </h4>
+								<p>If you are writing your PHP scripts using Windows Notepad, you will need to ensure that your files are saved with the .php extension. (Notepad adds a .txt extension to files automatically unless you take one of the following steps to prevent it.) <br>
+									<b>Company </b> - Orcons Systems Ltd <br>
+									<b>Duration </b> - 1yr [2017 to 2018] <br>
+									<b>Location </b> -  Achimota-ABC <br>
+								</p>
+							</li>
+							<li>
+								<h4><span> 2018 - </span> Java Developer </h4>
+								<p> In this tutorial we assume that your server has activated support for PHP and that all files ending in .php are handled by PHP. On most servers, this is the default extension for PHP files, but ask your server administrator to be sure. If your server supports PHP, then you do not need to do anything. <br>
+									<b>Company </b> - xyz company Ltd <br>
+									<b>Duration </b> - 1yr [2017 to 2018] <br>
+									<b>Location </b> -  Achimota <br>
+								</p>
+							</li>
+							<li>
+								<h4><span> 2017 - </span> C/C++ Developer </h4>
+								<p> You can either install these individually or choose a simpler way. Our manual has installation instructions for PHP (assuming you already have some web server set up) <br>
+									<b>Company </b> - xyz company Ltd <br>
+									<b>Duration </b> - 1yr [2017 to 2018] <br>
+									<b>Location </b> -  Achimota <br>
+								</p>
+							</li>
+						</ul>
+					</div>
+			
+
+			    	<div class="col-md-6">
+			    		<h3 class="text-center"> My Exducation </h3>
+			    		<ul class="timeline">
+			    			<li>
+			    				<h4><span> 2021 - </span>BSc. Computer Science </h4>
+			    				<p> If you are writing your PHP scripts using Windows Notepad, you will need to ensure that your files are saved with the .php extension. (Notepad adds a .txt extension to files automatically unless you take one of the following steps to prevent it.) <br>
+			    					<b>University</b> -University of Ghana <br>
+			    					<b>Session </b> - 4yrs [2017 to 2021] <br>
+			    					<b>Aggregate </b> -  90% <br>
+			    				</p>
+			    			</li>
+			    			<li>
+			    				<h4><span> 2016 - </span> Senior High </h4>
+			    				<p> In this tutorial we assume that your server has activated support for PHP and that all files ending in .php are handled by PHP. On most servers, this is the default extension for PHP files, but ask your server administrator to be sure. If your server supports PHP, then you do not need to do anything. <br>
+			    					<b>University</b> - Pope John  <br>
+									<b>Session </b> - 3yrs [2013 to 2016] <br>
+									<b>Aggregate </b> -  90% <br>
+								</p>
+							</li>
+							<li>
+								<h4><span> 2013 - </span> Junior High </h4>
+								<p> You can either install these individually or choose a simpler way. Our manual has installation instructions for PHP (assuming you already have some web server set up) <br>
+									<b>University</b> - Gohis <br>
+									<b>Session </b> - 4yrs [2010 to 2013] <br>
+									<b>Aggregate </b> -  90% <br>
+			           			</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+        </div> 
+	<!----------------Services-------------------------------->
+
+			<div class="services" id="services">
+				<div class="container">
+					<h1 class="text-center">My Offered Services </h1>
+					<p class="text-center" > You have obviously head of a number of programming languages out there; you may be wondering why we would want to use PHP as our poison for the web programming. Below are some of the compelling reasons</p>
+
+					<div class="row">
+						<div class="col-md-4">
+							<div class="services-box">
+								<i class="fa fa-rocket"></i> <span> Graphic </span>
+								<p> Hubtel's amazing APIs empower businesses and developers to build more than just applications. They empower businesses to sell and delight their customers everywhere.</p>
+							</div>
+						</div>
+
+						<div class="col-md-4">
+							<div class="services-box">
+								<i class="fa fa-line-chart"></i> <span> Marketing </span>
+								<p> Hubtel's amazing APIs empower businesses and developers to build more than just applications. They empower businesses to sell and delight their customers everywhere. </p>
+							</div>
+						</div>
+
+						<div class="col-md-4">
+							<div class="services-box">
+								<i class="fa fa-laptop"></i> <span> Development </span>
+								<p> Hubtel's amazing APIs empower businesses and developers to build more than just applications. They empower businesses 	to sell and delight their customers everywhere.</p>
+							</div>
+						</div> 
+					</div>
+				</div>
+			</div>
+
+
+<!-- -----Contact------------>
+
+			<div class="contact" id="contact">
+				<div class="container text-center">
+					<h1>Contact Me</h1>
+					<p class="text-center" > You have obviously head of a number of programming languages out there; you may be wondering why we would want to use PHP as our poison for the web programming. Below are some of the compelling reasons</p>
+
+					<div class="row">
+						<div class="col-md-4">
+							<i class="fa fa-phone"></i>
+							<p>0541001821</p>
+	   		   			</div>
+
+						<div class="col-md-4">
+							<i class="fa fa-envelope"></i>
+							<p>dnim331@gmail.com</p>
+	   		   			</div>		
+	    
+						<div class="col-md-4">
+							<i class="fa fa-internet-explorer"></i>
+							<p>www.mindfield.org</p>
+	   		   			</div>		
+	        		</div>
+				
+
+	        	<button type="button" class="btn btn-primary"> <i class="fa fa-download"></i> Resume </button>
+	        	<button type="button" class="btn btn-primary"> <i class="fa fa-rocket"></i>Hire Me </button>
+			</div>
+			</div>
+
+		<div class="footer text-center">
+			<p> Made with <i class="fa fa-heart-o"></i>  by MindField</p>
+		</div>
+
+</section>
+</body>
+</html>
